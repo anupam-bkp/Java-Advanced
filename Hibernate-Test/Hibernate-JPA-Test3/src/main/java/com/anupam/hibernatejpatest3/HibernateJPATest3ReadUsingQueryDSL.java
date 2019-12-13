bf93fbbc0b26389d62c7b83f616b1fb22cf72ce5
Anupam <NG61FEB@eu.airbus.corp>
2019-12-13 19:50:00 +0530

    QueryDSl related

--- Hibernate-Test/Hibernate-JPA-Test3/src/main/java/com/anupam/hibernatejpatest3/HibernateJPATest3ReadUsingQueryDSL.java
+++ Hibernate-Test/Hibernate-JPA-Test3/src/main/java/com/anupam/hibernatejpatest3/HibernateJPATest3ReadUsingQueryDSL.java
@@ -6,7 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.anupam.hibernatejpatest2.model.querydsl.QCustomer;
 import com.anupam.hibernatejpatest2.model.querydsl.QEmployee;
+import com.anupam.hibernatejpatest3.model.Customer;
 import com.anupam.hibernatejpatest3.model.Employee;
 import com.querydsl.jpa.impl.JPAQuery;
 
@@ -17,15 +19,24 @@ public class HibernateJPATest3ReadUsingQueryDSL {
 		final EntityManagerFactory emf = Persistence.createEntityManagerFactory("Hibernate-JPA-Test3A");
 		final EntityManager em = emf.createEntityManager();
 	
-		System.out.println("Read Started");
+		System.out.println("Reading Employee Data");
 		
 		JPAQuery<Employee> query = new JPAQuery<>(em);
-		
 		QEmployee qEmp = QEmployee.employee;
+		
 		List<Employee> fetch = query.from(qEmp).fetch();
+		fetch.stream().forEach(System.out :: println);
 		
-		fetch.stream().map(qEMP -> (Employee)qEMP).forEach(System.out :: println);
+		System.out.println("Reading Customer Data");
 		
+		JPAQuery<Customer> custQuery = new JPAQuery<>(em);
+		QCustomer qCust = QCustomer.customer;
+		
+		List<Customer> custList = custQuery.from(qCust).fetch();
+		for(Customer cust : custList) {
+			System.out.println(cust.getName());
+			System.out.println(cust.getProducts());
+		}	
 	}
 
 }
